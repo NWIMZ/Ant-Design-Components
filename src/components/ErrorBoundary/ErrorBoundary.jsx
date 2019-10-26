c61e912452d2b871429ed3d66f2fde8676786005
z <zw9409@126.com>
2019-10-26 10:16:40 +0800

    refactor(errorboundary): add errorView props

--- src/components/ErrorBoundary/ErrorBoundary.jsx
+++ src/components/ErrorBoundary/ErrorBoundary.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 export default class ErrorBoundary extends React.Component {
     state = { hasError: false }
+    static defaultProps = {
+        errorView: <p>组件渲染出错</p>
+    }
     static getDerivedStateFromError(error) {
         // 更新 state 使下一次渲染能够显示降级后的 UI
         return { hasError: true };
     }
 
     componentDidCatch(error, info) {
-        console.log(error, info);
+        console.error(error, info);
     }
 
     render() {
         if (this.state.hasError) {
             // 你可以自定义降级后的 UI 并渲染
-            return <p>组件渲染出错</p>;
+            return this.props.errorView;
         }
 
         return this.props.children;
